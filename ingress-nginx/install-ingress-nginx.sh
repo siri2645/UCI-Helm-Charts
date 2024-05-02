fc5389f86c935c2c2c6f4ff1003352f69bb74f65
siri2645 <168334583+siri2645@users.noreply.github.com>
2024-05-02 17:23:55 +0530

    Update install-ingress-nginx.sh

--- ingress-nginx/install-ingress-nginx.sh
+++ ingress-nginx/install-ingress-nginx.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
 helm upgrade --create-namespace --install ingress-nginx ingress-nginx/ingress-nginx -n ingress-nginx --values ./values-prod.yaml
