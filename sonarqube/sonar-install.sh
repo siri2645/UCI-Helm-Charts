faf94e1eb343af53bfcde5796d07671f29046062
siri2645 <168334583+siri2645@users.noreply.github.com>
2024-05-10 23:34:59 +0530

    Update sonar-install.sh

--- sonarqube/sonar-install.sh
+++ sonarqube/sonar-install.sh
@@ -5,4 +5,4 @@ set -x
 helm repo add sonarqube https://SonarSource.github.io/helm-chart-sonarqube
 helm repo update
 kubectl create ns sonarqube
-helm upgrade --install sonarqube sonarqube/sonarqube -n sonarqube
+helm upgrade --install sonarqube sonarqube/sonarqube -n sonarqube --values sonar-values.yaml
