c3ba9f872c79c3d2845b9942a912cc6795c760d1
root <root@ip-172-31-21-12.us-west-2.compute.internal>
2024-05-10 10:14:18 +0000

    added sonar

---
+++ sonarqube/sonar-install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -x
+
+helm repo add sonarqube https://SonarSource.github.io/helm-chart-sonarqube
+helm repo update
+kubectl create ns sonarqube
+helm upgrade --install sonarqube sonarqube/sonarqube -n sonarqube
