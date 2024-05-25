4c0c8edbda783bd602676993239ac74f55d98ca6
siri2645 <shireeshag2645@gmail.com>
2024-05-25 15:48:40 +0530

    add new rwpository

--- jenkins/install-jenkins.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-kubectl create -f ./gp2-encrypted-sc.yaml
-kubectl delete sc gp2
-helm repo add jenkins https://charts.jenkins.io
-helm repo update
-helm upgrade --create-namespace --install install jenkins jenkins/jenkins -n jenkins --values ./values-prod.yaml
--- secret-manager/secrets-deployments.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-kubectl create -f secret-store-csi-driver/
-kubectl create -f aws-provider-installer/
-kubectl create -f secret-provider-class.yaml
-kubectl patch sts sonarqube-sonarqube -n sonarqube --type=json -p='[{"op": "add", "path": "/spec/template/spec/containers/0/volumeMounts/-", "value": {"name": "rds-secret-volume", "mountPath": "/mnt/rds-secret", "readOnly": true}}]'
