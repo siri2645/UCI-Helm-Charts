3dad6df3e170b905a58c87761a0efe356f3d7161
lucky143225 <48136191+lucky143225@users.noreply.github.com>
2024-06-18 14:59:20 +0530

    Update jfrog-install.sh

--- jfrog/jfrog-install.sh
+++ jfrog/jfrog-install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+namespace=jfrog
+
 helm repo add jfrog https://charts.jfrog.io
 helm repo update
 
@@ -9,4 +11,28 @@ echo ${MASTER_KEY}
 export JOIN_KEY=$(openssl rand -hex 32)
 echo ${JOIN_KEY}
 
-helm upgrade --install artifactory --set artifactory.replicaCount=3 --set artifactory.masterKey=${MASTER_KEY} --set artifactory.joinKey=${JOIN_KEY} --namespace artifactory --create-namespace jfrog/artifactory
+helm upgrade --install artifactory --set artifactory.replicaCount=3 --set artifactory.masterKey=${MASTER_KEY} --set artifactory.joinKey=${JOIN_KEY} --namespace $namespace --create-namespace jfrog/artifactory --values jfrog-values.yaml
+
+POSTGRESQL_PASSWORD=$(kubectl get secret jfrogdb-externalsecrets -n $namespace -o jsonpath='{.data.postgres_password}' | base64 --decode)
+
+export POSTGRESQL_PASSWORD
+
+kubectl patch secret artifactory-postgres -n $namespace --type='json' -p='[{"op": "replace", "path": "/data/postgres-password", "value":"'"$POSTGRESQL_PASSWORD"'"}]'
+
+echo "artifactory-postgres secret updated successfully"
+
+PATTERNS=("artifactory-0" "artifactory-1" "artifactory-2")
+
+ALL_PODS=$(kubectl get pods -n $namespace -o jsonpath='{.items[*].metadata.name}')
+
+# Iterate over each pattern and delete matching pods
+for PATTERN in "${PATTERNS[@]}"; do
+  for POD in $ALL_PODS; do
+    if [[ $POD == *$PATTERN* ]]; then
+      echo "Deleting pod: $POD"
+      kubectl delete pod $POD -n $namespace
+    fi
+  done
+done
+
+echo "All specified pods in the $namespace have been deleted."
