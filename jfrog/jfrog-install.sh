340caec95604574c6e663c93fc432acdba5dc8e4
siri2645 <168334583+siri2645@users.noreply.github.com>
2024-07-04 11:48:36 +0530

    Create jfrog-install.sh

---
+++ jfrog/jfrog-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+namespace="jfrog"
+
+# Add JFrog Helm repo and update
+helm repo add jfrog https://charts.jfrog.io
+helm repo update
+
+# Install/Upgrade Artifactory with Helm
+helm upgrade --install artifactory --set artifactory.replicaCount=1 --set --namespace $namespace --create-namespace jfrog/artifactory --values jfrog-values.yaml
