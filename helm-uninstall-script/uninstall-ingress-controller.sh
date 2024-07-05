bf2ff88c4c2aad5051428bf151256f8e77e146bd
siri2645 <168334583+siri2645@users.noreply.github.com>
2024-07-05 01:26:01 -0700

    Update uninstall-ingress-controller.sh

--- helm-uninstall-script/uninstall-ingress-controller.sh
+++ helm-uninstall-script/uninstall-ingress-controller.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 helm uninstall ingress-nginx -n ingress-nginx
 kubectl delete ns ingress-nginx
+
+
+https://jfrog.com/help/r/jfrog-release-information/artifactory-7.77?tocId=ghe~Akmc_XXzvccCaFuUew
+https://www.scmgalaxy.com/tutorials/artifactory-error-error-1071-specified-key-was-too-long-max-key-length-is-3072-bytes/
